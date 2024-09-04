7c57db69d9b4f0c1d31487d404a4bf4f2e3f74f4
Shiulan <shiulan.t@gmail.com>
2024-09-04 16:58:39 +0200

    les 17

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ const app = Vue.createApp({
             console.log(book.isFav)
             book.isFav = !book.isFav
         }
+    },
+    computed: {
+        filteredBooks() {
+            return this.books.filter((book) => book.isFav)
+        }
     }
 })
 
