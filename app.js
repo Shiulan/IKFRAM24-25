a73a9980e46008d1607ba026be3c5ed272e578c7
Shiulan <shiulan.t@gmail.com>
2024-09-04 16:17:01 +0200

    les 11

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'Titel',
             description: 'test',
-            age: 40
+            age: 40,
+            x: 0,
+            y: 0
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        handleEvent(event, param1) {
+            console.log(event, event.type)
+            if(param1){
+                console.log(param1)
+            }
+        },
+        handleMouseMove(event) {
+            this.x = event.offsetY
+            this.y = event.offsetY
         }
     }
 })
